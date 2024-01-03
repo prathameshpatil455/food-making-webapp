879c7b20d4852e7fd50a35d50aba7c872286fc54
prathameshpatil455 <132359689+prathameshpatil455@users.noreply.github.com>
2024-01-03 23:43:06 +0530

    added api and links

--- src/Components/MealItem.jsx
+++ src/Components/MealItem.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-
-const MealItem = ({data}) => {
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const MealItem=({data})=>{
     console.log(data);
-  return (
-    <>
-        {
-            (data) ? data.map(item => {
-                return (
-                    <div className="card">
+    let navigate = useNavigate();
+    return(
+        <>  
+            {
+                (!data) ?"Not Found": data.map(item=>{
+                    return(
+                    <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
                         <img src={item.strMealThumb} alt="" />
                         <h3>{item.strMeal}</h3>
                     </div>
-                )
-                 
-            }) : "Not Found"
-        }
-        
-    </>
-  )
+                    )
+                })
+            }
+            
+        </>
+    )
 }
-
 export default MealItem;
